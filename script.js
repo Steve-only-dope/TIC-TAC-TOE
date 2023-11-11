45724acf66d53efd9efb9d416dce793cff359ba2
Steve-only-dope <134184727+Steve-only-dope@users.noreply.github.com>
2023-11-10 20:53:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let currentPlayer = "X";
+let currentPlayer = "🔴";
 let moves = 0;
 let gameActive = true;
-let scores = { "X": 0, "O": 0 };
+let scores = { "🔴": 0, "🟡": 0 };
 
-function cellClick(cell) {
+function makeMove(cell) {
   if (gameActive && !cell.innerHTML) {
+    cell.classList.add(currentPlayer === "🔴" ? 'x' : 'o');
     cell.innerHTML = currentPlayer;
     moves++;
     if (checkWinner()) {
@@ -22,7 +23,7 @@ function cellClick(cell) {
         resetGame();
       }, 100);
     } else {
-      currentPlayer = currentPlayer === "X" ? "O" : "X";
+      currentPlayer = currentPlayer === "🔴" ? "🟡" : "🔴";
     }
   }
 }
@@ -31,35 +32,7 @@ function checkWinner() {
   const grid = document.getElementById('grid');
   const cells = grid.getElementsByClassName('cell');
   const winningCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
-  ];
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+    [0, 4, 8], [2, 4,
 
-  for (const combination of winningCombinations) {
-    const [a, b, c] = combination;
-    if (cells[a].innerHTML && cells[a].innerHTML === cells[b].innerHTML && cells[a].innerHTML === cells[c].innerHTML) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-function resetGame() {
-  const grid = document.getElementById('grid');
-  const cells = grid.getElementsByClassName('cell');
-
-  for (const cell of cells) {
-    cell.innerHTML = '';
-  }
-
-  currentPlayer = "X";
-  moves = 0;
-  gameActive = true;
-}
-
-function updateScore() {
-  document.getElementById('scoreX').innerText = "Score X: " + scores["X"];
-  document.getElementById('scoreO').innerText = "Score O: " + scores["O"];
-}
