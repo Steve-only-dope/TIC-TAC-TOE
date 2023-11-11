5c85fa852169dc1b84b247c8f4c344618622ca00
Steve-only-dope <134184727+Steve-only-dope@users.noreply.github.com>
2023-11-10 21:01:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -34,5 +34,39 @@ function checkWinner() {
   const winningCombinations = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
     [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
-    [0, 4, 8], [2, 4,
+    [0, 4, 8], [2, 4, 6] // diagonals
+  ];
 
+  for (const combination of winningCombinations) {
+    const [a, b, c] = combination;
+    if (cells[a].classList.contains('x') &&
+      cells[a].classList.contains('x') === cells[b].classList.contains('x') &&
+      cells[a].classList.contains('x') === cells[c].classList.contains('x')) {
+      return true;
+    } else if (cells[a].classList.contains('o') &&
+      cells[a].classList.contains('o') === cells[b].classList.contains('o') &&
+      cells[a].classList.contains('o') === cells[c].classList.contains('o')) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function resetGame() {
+  const grid = document.getElementById('grid');
+  const cells = grid.getElementsByClassName('cell');
+  for (const cell of cells) {
+    cell.innerHTML = '';
+    cell.classList.remove('x', 'o');
+  }
+
+  currentPlayer = "🔴";
+  moves = 0;
+  gameActive = true;
+}
+
+function updateScore() {
+  document.getElementById('scoreX').innerText = "Score 🔴: " + scores["🔴"];
+  document.getElementById('scoreO').innerText = "Score 🟡: " + scores["🟡"];
+}
